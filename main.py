15f0925a4fd440b0e707df74796c6f6689f185c8
mirrroxx <astakhov2547@yandex.ru>
2025-09-17 14:53:06 +0700

    фигня

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST', 'GET'])
 def main():
-    print(request.json['message']['text'])
+    print(request.json)
     return "52"
 
 

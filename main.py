a4a287dfdd17435178396a1351d8e263d06c954b
mirrroxx <astakhov2547@yandex.ru>
2025-09-18 14:55:59 +0700

    базоваый функционал

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
+import asyncio
 import logging
-from telegram import InlineKeyboardMarkup, Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler, CallbackQueryHandler
+import sys
+from os import getenv
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+from aiogram import Bot, Dispatcher, html, types, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("8443997188:AAG4NphJAlYCRrgELAmq-WsL4xmyoQBYBMM")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Sends a message with three inline buttons attached."""
-    keyboard = [
-        [
-            InlineKeyboardButton("Option 1", callback_data="1"),
-            InlineKeyboardButton("Option 2", callback_data="2"),
-        ],
-        [InlineKeyboardButton("Option 3", callback_data="3")],
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+kb = [
+        [types.KeyboardButton(text="Ознакомиться с правилами")],
+        [types.KeyboardButton(text="Я ознакомился с правилами")]
     ]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
-    
-    
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)    
-    
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer(f"Привет, {message.from_user.full_name}!\n\nЭто бот для занятия очереди на стирку факультета ИИР\n\nПеред началом работы с ботом ознакомься с правилами пользования прачечной", reply_markup=keyboard)
     
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('8443997188:AAG4NphJAlYCRrgELAmq-WsL4xmyoQBYBMM').build()
+@dp.message(F.text.lower() == 'ознакомиться с правилами')
+async def pravila(message: Message):
+    await message.reply('НЕЛЬЗЯ ПИХАТЬ ХУЙ В СТИРАЛКУ!')
     
-    start_handler = CommandHandler('start', start)
-    hello_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, hello)
-    application.add_handler(start_handler)
-    application.add_handler(hello_handler)
-    
-    application.run_polling()
+
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token='8443997188:AAG4NphJAlYCRrgELAmq-WsL4xmyoQBYBMM', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
